6eacd45fa775fe442832bd453c6e19568ec1ccb2
Siegfried <42670113+wintone123@users.noreply.github.com>
2020-03-09 12:45:49 +0100

    1

--- Circlize/test2.R
+++ Circlize/test2.R
@@ -1,18 +1,61 @@
-circos.clear()
-circos.genomicInitialize(rna2)
-circos.genomicTrackPlotRegion(rna2, ylim = c(0,1), track.height = 0.05, bg.border = 'white',
-                              panel.fun = function(region, value, ...) {
-                                for (i in (1: nrow(region))) {
-                                  reg <- region[i,]
-                                  val <- value[1,]
-                                  color <- ifelse(i %% 2 == 1, 'black', 'grey')
-                                  circos.genomicRect(reg, val, col = color, border = 'white')
-                                }
-                              })
-circos.genomicLink(links_from, links_to, col = rand_color(nrow(links_from), transparency = 0.7))
+interaction_graph <- function(genome, from, to) {
+  circos.clear()
+  circos.genomicInitialize(genome)
+  # counts show as histograms
+  circos.genomicTrackPlotRegion(genome, ylim = c(0,1), track.height = 0.1, bg.border = 'white',
+                                panel.fun = function(region, value, ...) {
+                                  for (i in (1: nrow(region))) {
+                                    reg <- region[i,]
+                                    val <- value[i,] / max(value[1])
+                                    circos.genomicRect(reg, val, ybottom = 0, ytop = val, border = 'white', 
+                                                       col = ifelse(val >= 0.5, 'red', 'green'))
+                                  }
+                                })
+  # genome tracks
+  circos.genomicTrackPlotRegion(genome, ylim = c(0,1), track.height = 0.05, bg.border = 'white',
+                                panel.fun = function(region, value, ...) {
+                                  for (i in (1: nrow(region))) {
+                                    reg <- region[i,]
+                                    val <- value[i,]
+                                    color <- ifelse(i %% 2 == 1, 'black', 'grey')
+                                    circos.genomicRect(reg, val, col = color, border = 'white')
+                                  }
+                                })
+  # genome links
+  circos.genomicLink(from, to)
+}
+
+
+interaction_graph <- function(genome, links) {
+  circos.clear()
+  circos.genomicInitialize(genome)
+  # counts show as vertical lines
+  circos.genomicTrackPlotRegion(genome, ylim = c(0,1), track.height = 0.1, bg.border = 'white',
+                                panel.fun = function(region, value, ...) {
+                                  for (i in (1: nrow(region))) {
+                                    reg <- region[i,]
+                                    val <- value[i,] / max(value[1])
+                                    circos.genomicLines(reg, val, numeric.column = 1, type = 'h', lwd = 1,
+                                                        col = ifelse(val >= 0.5, 'red', 'green'))
+                                  }
+                                })
+  # genome tracks
+  circos.genomicTrackPlotRegion(genome, ylim = c(0,1), track.height = 0.05, bg.border = 'white',
+                                panel.fun = function(region, value, ...) {
+                                  for (i in (1: nrow(region))) {
+                                    reg <- region[i,]
+                                    val <- value[i,]
+                                    color <- ifelse(i %% 2 == 1, '#FF00FF60', 'grey')
+                                    circos.genomicRect(reg, val, col = color, border = NA)
+                                  }
+                                })
+  # genome links
+  circos.genomicLink(links[1:3], links[4:6])
+}
+
 
 # arrange
-re_genome <- function(df, bin){
+re_genome <- function(df, bin) {
   end <- vector()
   for (i in range(1, nrow(df))) {
     end <- append(end, (df$end[i] - df$start[i]) %/% bin + 1)
@@ -24,7 +67,7 @@ re_genome <- function(df, bin){
 
 
 # arrange start & end as bin position
-re_position <- function(df, start, bin){
+re_position <- function(df, start, bin) {
   start_bin <- vector()
   end_bin <- vector()
   for (i in range(1, nrow(df))) {
